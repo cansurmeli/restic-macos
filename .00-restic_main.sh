fa2b7daace082481e41ee71275b0e8342bb098d4
Can Sürmeli <c_surmeli@icloud.com>
2019-05-28 09:44:49 +0200

    Reflected the naming changes to the main backup script.

--- .00-restic_main.sh
+++ .00-restic_main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source .restic_credentials
-sh ./restic_backup.sh
-sh ./restic_forget.sh
-sh ./restic_check.sh
+source .01-restic_credentials
+sh ./02-restic_backup.sh
+sh ./03-restic_forget.sh
+sh ./04-restic_prune.sh
+sh ./05-restic_check.sh
