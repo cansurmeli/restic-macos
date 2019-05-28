bb3d45bc38ce7db4823d45d659a46eb9e467eff1
Can Sürmeli <c_surmeli@icloud.com>
2019-05-28 09:49:02 +0200

    Improved the scrpting reachability.
    Brought along with it yet again another namign scheme change.

--- .00-restic_main.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source .01-restic_credentials
-sh ./02-restic_backup.sh
-sh ./03-restic_forget.sh
-sh ./04-restic_prune.sh
-sh ./05-restic_check.sh
---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+source .00-restic_credentials
+sh ./.01-restic_backup.sh
+sh ./.02-restic_forget.sh
+sh ./.03-restic_prune.sh
+sh ./.04-restic_check.sh
