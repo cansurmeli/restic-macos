e975dc91dd171c33efeed2edb81887dc1ce5d96d
Can Sürmeli <cansurmeli@outlook.com>
2022-11-18 11:32:57 +0000

    Improved the in-file documentation of `restic check`.

--- .11-restic_check.sh
+++ .11-restic_check.sh
@@ -1,3 +1,19 @@
-# --with-cache - limits Class B Transactions on BackBlaze B2
-# https://forum.restic.net/t/limiting-b2-transactions/209/4
-restic check --with-cache
+
+#   The `check` command of restic tests the repository against
+# errors and reports if any are found.
+#
+#   By default, this command loads all data from the repository
+# and not uses any local data. This is done on purpose since
+# restic is built for safety and security.
+#
+#   However, this situation can be changed with the flag
+# `--with-cache` which will only download metadata files and
+# run checks on them. This will be more economical in terms of
+# your Backblaze bill at the end of the month since Backblaze's
+# developer package is billed based on the amount of download
+# and upload.
+#
+#   RESOURCE(s)
+#   - https://forum.restic.net/t/limiting-b2-transactions/209/4
+
+restic check --with-cache   # limits Class B Transactions on BackBlaze B2
